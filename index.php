963c27864bb12eae05365de4d9035c8a04699648
smekker11 <mekkerdavid@gmail.com>
2022-12-03 17:54:31 +0200

    Text cultura egipteana.

--- index.php
+++ index.php
@@ -55,11 +55,33 @@
 </div>
 <!--TEXTUS-->
 <div id="textus">
-  <h3 class='ht'>Horus,vine de la cuvântul "Ho"- care în traducere liberă înseamnă "față, chip", fiul lui Isis și al lui Osiris, zeu protector al Egiptului, este reprezentat cu cap de șoim.</h3><img class='it' height="100" width="120" src="lemon.png"></img>
-  <h3 class='ht'>Ankh este o hieroglifă egipteană, simbol a vieții lungi. Faraonii primeau o amuletă sub formă de ankh la naștere. De asemenea Ankh-ul era folosit pentru protecție împotriva spiritelor rele.</h3><img class='it' height="100" width="120" src="orange.jpg"></img>
-  <h3 class='ht'>Masca lui Tutankhamon este o mască de aur a faraonului egiptean Tutankhamon din Dinastia a XVIII-a. A fost descoperită de Howard Carter în 1925 in Valea Regilor.</h3><img class='it' height="100" width="120" src="cherry.jpg"></img>
-  <h3 class='ht'>Piramidele din Giza complex de monumente antice, se numără printre cele mai cunoscute piramide din antichitate, ele fiind considerate una dintre cele șapte minuni ale lumii antice.</h3><img class='it' height="100" width="120" src="seven.png"></img>
-  <h3 class='ht'>Anubis este denumirea în greaca clasică a zeului egiptean cu cap de șacal asociat mumificării și vieții de apoi. În limba egipteană veche Anubis era cunoscut ca Inpu și protectorul mormintelor.</h3><img class='it' height="100" width="120" src="plum.png"></img>
+  <h3 class='ht'>Horus,vine de la cuvântul "Ho"- care în traducere liberă înseamnă "față, chip", fiul lui Isis și al lui Osiris, zeu protector al Egiptului, este reprezentat cu cap de șoim.
+  Horus a fost născut de zeița Isis. Însărcinată cu Horus, Isis a fugit în mlaștinile din Delta Nilului ca să se ascundă de fratele ei Seth, cel care l-a ucis pe Osiris din gelozie și care voia să-l omoare și pe fiul lor. Horus era sărbătorit in luna Noiembrie odată cu începerea noului an.
+  Ochiul lui Horus(numit si Wedjat sau Udjat pe limba veche egipteana) este un simbol antic egiptean, de protecție si de putere regală. Acest simbol sacru se regăsește pe aproape toate operele de artă. Era considerat o sursă de fluid magic, ochiul-lumină purificator.
+  Horus a fost adorat vreme îndelungată ca zeu al libertății spațiilor aeriene; devenit mai târziu Zeul-soare, începe să fie socotit domnitor peste cer și aștri; apoi ajunge protectorul personal al faraonului sau chiar se confruntă cu faraonul însuși.
+  </h3><img class='it' height="100" width="120" src="lemon.png"></img>
+  <h3 class='ht'>Ankh este o hieroglifă egipteană, simbol a vieții lungi. Faraonii primeau o amuletă sub formă de ankh la naștere. De asemenea Ankh-ul era folosit pentru protecție împotriva spiritelor rele. 
+  Simbolul Ankh-ului nu se limitează la viața materială, ci trece mai departe de ea, simbolizând viața în moarte. De aceea, morții erau numiți “ankhu” și un termen pentru sarcofag era “neb-ankh” care însemna “posesor al vieții”.
+  Ankh-ul nu numai că simbolizează Viața Eternă (atât cea materială cât și cea spirituală), însă reprezintă și puterea de a da și susține viața. Este asociat în general cu lucrurile materiale precum apa (pe care egiptenii o considerau capabilă de a regenera viața), aerul, soarele precum și cu zeii.
+
+De-a lungul timpurilor, Ankh-ul a devenit simbolul Vieții și Imortalității, al Universului, al puterii, al apei și aerului dătători de viață. Forma sa asemănătoare cu o cheie a încurajat credința că poate deschide porțile morții.
+
+Ankh-ul este popular și în rândul Păgânilor, în mare parte datorită asemănării cu Crucea Creștină ce face ca Ankh-ul să fie un simbol mai puțin șocant comparat cu o pentagramă.
+  </h3><img class='it' height="100" width="120" src="orange.jpg"></img>
+  <h3 class='ht'>Masca lui Tutankhamon este o mască de aur a faraonului egiptean Tutankhamon din Dinastia a XVIII-a. A fost descoperită de Howard Carter în 1925 in Valea Regilor.
+  Camera de înmormântare a lui Tutankhamun a fost găsită la Necropola Tebană din Valea Regilor în 1922 și a fost deschisă în 1923. Ar fi trecut încă doi ani înainte ca echipa de excavare, condusă de arheologul englez Howard Carter, să poată să deschidă sarcofagul greu care conține mumia lui Tutankhamon. Pe 28 octombrie 1925, aceștia au deschis interiorul a trei sarcofage ca să descopere masca de aur, văzută de oameni pentru prima dată în aproximativ 3.250 de ani.
+  Semănând cu Osiris, zeul egiptean al vieții de apoi, masca are 54 de centimetri înălțime, cântărește peste 10 kilograme și este decorată cu pietre semi-prețioase. O vrajă străveche din Cartea Morților este scrisă cu hieroglife pe umerii măștii. Masca a trebuit să fie restaurată în 2015 după ce barba împletită de 2,5 kilograme a căzut și a fost lipită în grabă de lucrătorii muzeului.
+  În decembrie 1925, masca a fost scoasă din mormânt, așezată într-o ladă și transportată 635 de kilometri la Muzeul Egiptean din Cairo, unde a rămas expusă publicului.
+  </h3><img class='it' height="100" width="120" src="cherry.jpg"></img>
+  <h3 class='ht'>Piramidele din Giza complex de monumente antice, se numără printre cele mai cunoscute piramide din antichitate, ele fiind considerate una dintre cele șapte minuni ale lumii antice.
+  Piramidele din Giza sunt alcătuite din Marea Piramidă din Giza (cunoscută și sub denumirea de Piramida din Cheops sau Khufu și construită în jurul anului 2580 - circa 2560 î.Hr.), oarecum mai mică, Piramida lui Khafra (sau Chephren) la câteva sute de metri spre sud -vest și, relativ modestă, Piramida Menkaura (sau Mykerinos) la câteva sute de metri mai departe spre sud-vest. Marele Sfinx se află pe partea de est a complexului. Consensul actual printre egiptologi este că capul Marelui Sfinx este cel al lui Khafra. Împreună cu aceste monumente majore există un număr de edificii satelit mai mici, cunoscute ca piramidele "regine", drumuri pietruite și piramidele vale.
+  </h3><img class='it' height="100" width="120" src="seven.png"></img>
+  <h3 class='ht'>Anubis este denumirea în greaca clasică a zeului egiptean cu cap de șacal asociat mumificării și vieții de apoi. În limba egipteană veche Anubis era cunoscut ca Inpu și protectorul mormintelor.
+  Anubis este deseori descris ca fiind un bărbat de culoare neagră cu cap de șacal sau ca fiind un șacal negru. Egiptologul Flinders Petrie consideră conceptul lui Anubis astfel: egiptenii observând șacalii pe lângă morminte, au asociat șacalul cu omul creându-l astfel pe zeu. Pentru a arăta importanța sa în lumea morților, capul său era pictat cu negru, aceasta fiind și o reprezentare a fertilității. Anubis era considerat, pe lângă ghidul sufletelor decedaților, ca fiind zeul îmbălsămării și paznicul mormintelor.
+  Egiptenii credeau că Anubis este inventatorul mumificării datorită faptul că acesta a ajutat-o pe Isis să își readucă la viață soțul, după ce Seth îl omorâse, bandajând corpul zeului în haine de pânză de in țesute de Isis și Nephthys pentru a fi sigur că acestea nu vor putrezi.
+  Deoarece egiptenii nu făceau o deosebire mare dintre șacali și câini, există posibilitatea confuziei între aceste animale. Deși despre Anubis se face deseori referire prin cuvântul sab(ce însemna șacal) mai degrabă decât iwiw(ce însemna câine), este încă nesigură specia canină din care facea parte.
+  Multe desene prezente prin monumentele antice ale egiptenilor il arata de multe ori pe Anubis cu o amuleta Ankh in mana, acest lucru intareste simbolistica amuletei care face legatura intre viata moarte cer si pamant.
+  </h3><img class='it' height="100" width="120" src="plum.png"></img>
 </div>
 <!--iframus-->
 <div id="iframus">
@@ -75,6 +97,7 @@
   <h4 class="ht">Tur virtual piramida din giza: https://youtu.be/TMzouTzim0o</h4>
   <h4 class="ht">Piramida vazuta cu drona in 4K: https://youtu.be/dz8qoMeNlBs</h4>
   <h4 class="ht">Toate pozele/pictogramele/imaginile acestui proiect provin de pe Wikipedia si respecta politicile Wikipedia Commons pentru utilizarea materialelor digitale.</h4>
+  <h4 class="ht">Multe texte referitoare la zei si simbolistica egipteana au fost preluate tot de pe Wikipedia.</h4>
 </div>
 <div id="hr-container"><hr class="line" id="hr1"><hr class="line" id="hr2"><hr class="line" id="hr3"><hr class="line" id="hr4"><hr class="line" id="hr5"></div>
 </body>
